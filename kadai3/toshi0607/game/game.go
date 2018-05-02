7c13fa81e2022eed4054a76dbe7c7decbd4914a3
toshi0607 <s.toshi0607@gmail.com>
2018-05-02 10:53:55 +0900

    :sparkles: press any key to start

--- kadai3/toshi0607/game/game.go
+++ kadai3/toshi0607/game/game.go
@@ -15,6 +15,9 @@ type Game struct {
 }
 
 func (g *Game) Run() int {
+	sch := start(os.Stdin)
+	<-sch
+
 	ch := input(os.Stdin)
 
 	words, err := word.GetWords()
@@ -54,19 +57,35 @@ Outer:
 	fmt.Printf("正解数: %d words\n", correctAnswerCount)
 	correctness := float64(0)
 	if answerCount != 0 {
-		correctness = RoundPlus(float64(correctAnswerCount) / float64(answerCount) * 100, 2)
+		correctness = roundPlus(float64(correctAnswerCount) / float64(answerCount) * 100, 2)
 	}
 	fmt.Printf("正確さ: %v %%\n", correctness)
 	return 0
 }
 
-func Round(f float64) float64 {
+func round(f float64) float64 {
 	return math.Floor(f + .5)
 }
 
-func RoundPlus(f float64, places int) (float64) {
+func roundPlus(f float64, places int) (float64) {
 	shift := math.Pow(10, float64(places))
-	return Round(f * shift) / shift;
+	return round(f * shift) / shift;
+}
+
+func start(r io.Reader) <-chan struct{} {
+	fmt.Println("\nタイピングゲームやで")
+	fmt.Println("\n制限時間は30秒")
+	fmt.Println(">>> press any key to start <<<")
+	ch := make(chan struct{})
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- struct{}{}
+			break
+		}
+		close(ch)
+	}()
+	return ch
 }
 
 func input(r io.Reader) <-chan string {
