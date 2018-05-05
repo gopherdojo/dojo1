d83854bb8a68110966defbf43c9b1a6ec61badcb
itosho <sho.ito@supership.jp>
2018-05-05 23:53:58 +0900

    add typing package

--- kadai3/itosho/typingo/main.go
+++ kadai3/itosho/typingo/main.go
@@ -3,25 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"math/rand"
 	"os"
-	"time"
+
+	"./typing"
 )
-
-func input(r io.Reader) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		s := bufio.NewScanner(r)
-		for s.Scan() {
-			ch <- s.Text()
-		}
-		close(ch)
-	}()
-
-	return ch
-}
 
 func main() {
 	fmt.Println("Start typingo game!")
@@ -43,49 +28,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	questionCnt := 0
-	correctCnt := 0
-	wrongCnt := 0
-
-	t := time.After(10 * time.Second)
-	ch := input(os.Stdin)
-	for {
-		select {
-		case <-t:
-			fmt.Println("time over.")
-			fmt.Println("-")
-			fmt.Println(fmt.Sprintf("result: %d/%d", correctCnt, questionCnt))
-			return
-		default:
-			questionCnt++
-
-			question := getRandWord()
-			fmt.Println("-")
-			fmt.Println(question)
-			answer := <-ch
-
-			if question == answer {
-				correctCnt++
-				fmt.Println("correct!")
-			} else {
-				wrongCnt++
-				fmt.Println("wrong!")
-			}
-		}
-	}
-}
-
-func getRandWord() string {
-	words := [...]string{
-		"dog",
-		"cat",
-		"elephant",
-		"lion",
-		"bird",
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	i := rand.Intn(len(words))
-
-	return words[i]
+	typing.Start()
 }
---
+++ kadai3/itosho/typingo/typing/typing.go
@@ -0,0 +1,72 @@
+package typing
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+func Start() {
+	questionCnt := 0
+	correctCnt := 0
+	wrongCnt := 0
+
+	t := time.After(10 * time.Second)
+	ch := input(os.Stdin)
+	for {
+		select {
+		case <-t:
+			fmt.Println("time over.")
+			fmt.Println("-")
+			fmt.Println(fmt.Sprintf("result: %d/%d", correctCnt, questionCnt))
+			return
+		default:
+			questionCnt++
+
+			question := getRandWord()
+			fmt.Println("-")
+			fmt.Println(question)
+			answer := <-ch
+
+			if question == answer {
+				correctCnt++
+				fmt.Println("correct!")
+			} else {
+				wrongCnt++
+				fmt.Println("wrong!")
+			}
+		}
+	}
+}
+
+func getRandWord() string {
+	words := [...]string{
+		"dog",
+		"cat",
+		"elephant",
+		"lion",
+		"bird",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(words))
+
+	return words[i]
+}
