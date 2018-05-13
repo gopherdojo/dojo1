709d55d1ee01ce6434f866c03e4c1168e9a82819
山田雄太 <yamadayuuta@yamadayuutanoMacBook-Pro.local>
2018-05-13 19:09:19 +0900

    エラー処理を追加

--- kadai3/yuuyamad/main.go
+++ kadai3/yuuyamad/main.go
@@ -4,68 +4,28 @@ import (
 	"os"
 	"net/http"
 	"strconv"
-	"sync"
 	"io/ioutil"
 	"fmt"
 	"io"
+	"golang.org/x/sync/errgroup"
+	"context"
+	"time"
 )
+
+const LIMIT=10
+
+type Range struct {
+	low    int
+	high   int
+	worker int
+}
 
 func main() {
 
-	url := os.Args[1]
-
-	// contents Headerを取得する
-	res, err := http.Head(url)
+	err := download()
 	if err != nil {
 		logError(err)
 	}
-
-	maps := res.Header
-	length, err := strconv.Atoi(maps["Content-Length"][0])
-
-	limit := 10
-
-	len_sub := length/limit
-	diff := length%limit
-	body := make([]string, 11)
-
-	wg := &sync.WaitGroup{}
-
-	for i:=0; i < limit; i++ {
-		wg.Add(1)
-
-		min := len_sub * i
-		max := len_sub * (i + 1)
-
-		if(i == limit - 1){
-			max += diff
-		}
-
-		go func(min int, max int, i int) {
-			client := &http.Client{}
-			req , err := http.NewRequest("GET", url, nil)
-			if err != nil {
-				logError(err)
-			}
-			range_header := "bytes=" + strconv.Itoa(min) + "-" + strconv.Itoa(max-1)
-			req.Header.Add("Range", range_header)
-			resp,err := client.Do(req)
-			if err != nil {
-				logError(err)
-			}
-			defer resp.Body.Close()
-
-
-			reader, err := ioutil.ReadAll(resp.Body)
-			body[i] = string(reader)
-			ioutil.WriteFile("hoge.png." + strconv.Itoa(i), []byte(string(body[i])), 0x777)
-
-			wg.Done()
-
-
-		}(min, max, i)
-	}
-	wg.Wait()
 
 	//分割ダウンロードしたファイルを結合する
 	fh, err := os.Create("hoge.png")
@@ -74,7 +34,7 @@ func main() {
 	}
 	defer fh.Close()
 
-	for j:=0; j < limit; j++ {
+	for j:=0; j < LIMIT; j++ {
 		f := fmt.Sprintf("%s.%d", "hoge.png", j)
 		subfp, err := os.Open(f)
 		if err != nil{
@@ -88,4 +48,75 @@ func main() {
 			logError(err)
 		}
 	}
+}
+func download() error{
+	url := os.Args[1]
+
+	// contents Headerを取得する
+	res, err := http.Head(url)
+	if err != nil {
+		logError(err)
+	}
+
+	maps := res.Header
+	length, err := strconv.Atoi(maps["Content-Length"][0])
+
+	len_sub := length/LIMIT
+	diff := length%LIMIT
+
+	// errorGroup
+	grp, ctx := errgroup.WithContext(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+
+	for i:=0; i < LIMIT; i++ {
+
+		min := len_sub * i
+		max := len_sub * (i + 1)
+
+		r := Range{}
+		r.low = min
+		r.high = max
+		r.worker = i
+
+		if(i == (LIMIT - 1)){
+			max += diff
+		}
+		// execute get request
+		grp.Go(func() error {
+			return requests(ctx, url, r)
+		})
+
+		fmt.Println(i)
+	}
+	if err := grp.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func requests(ctx context.Context, url string, r Range) error {
+	body := make([]string, 11)
+	client := &http.Client{}
+	req , err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+
+	range_header := "bytes=" + strconv.Itoa(r.low) + "-" + strconv.Itoa(r.high-1)
+	req.Header.Add("Range", range_header)
+	resp,err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	reader, err := ioutil.ReadAll(resp.Body)
+	body[r.worker] = string(reader)
+	fmt.Println(r.worker)
+	ioutil.WriteFile("hoge.png." + strconv.Itoa(r.worker), []byte(string(body[r.worker])), 0x777)
+
+	return nil
 }
