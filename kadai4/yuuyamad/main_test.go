716267e0c9d10462de574fd04a0c25348c435f84
山田雄太 <yamadayuuta@yamadayuutanoMacBook-Pro.local>
2018-05-31 08:24:35 +0900

    テストコード修正

--- kadai4/yuuyamad/main_test.go
+++ kadai4/yuuyamad/main_test.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"fmt"
 )
 
 const timeformat = "2006-01-02 15:04:06"
@@ -31,4 +32,22 @@ func TestGetOmikujiain(t *testing.T) {
 	if err != nil { t.Fatal("unexpected error") }
 	const expected = "{\"omikuji\":\"大吉\"}"
 	if s := string(b); !assert.JSONEq(t, s, expected) { t.Fatalf("unexpected response: %s", s) }
+
+
+	//吉がでるseed
+	date , _ := time.Parse(timeformat, "2018-02-03 14:10:00")
+	setNow(date)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("GET", "/api/v1/omikuji", nil)
+	GetOmikuji(w, r)
+	rw = w.Result()
+	defer rw.Body.Close()
+
+	if rw.StatusCode != http.StatusOK { t.Fatal("unexpected status code") }
+	b, err = ioutil.ReadAll(rw.Body)
+	fmt.Println(string(b))
+	if err != nil { t.Fatal("unexpected error") }
+	const expected2 = "{\"omikuji\":\"吉\"}"
+	if s := string(b); !assert.JSONEq(t, s, expected2) { t.Fatalf("unexpected response: %s", s) }
 }
